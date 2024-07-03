447c67278230acb19ddc8f7c3241ff056e8cb6f6
Matt <34448412+mattops@users.noreply.github.com>
2024-07-03 10:37:14 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  kotlin("plugin.spring") version "2.0.0"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+kotlin {
+  jvmToolchain(21)
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+  }
+}
